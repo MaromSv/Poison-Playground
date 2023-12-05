44c2a0499871af66709f652cdf1cc1cdd863c3ee
MaromSv <118516370+MaromSv@users.noreply.github.com>
2023-12-05 08:42:28 +0100

    dataset.py should be fixed

--- Federated-Learning/dataset.py
+++ Federated-Learning/dataset.py
@@ -18,19 +18,15 @@ class Dataset :
         
         self.num_clients = num_clients
 
-        self.horizontal_clients_dataset = self.horizontalDivideData()
+        self.horizontal_clients_datasets = self.horizontalDivideData()
 
         self.vertical_clients_datasets = self.verticalDivideData()
 
-        
-        
-        
-
 
     def horizontalDivideData(self):
-        # Shuffle the data to introduce randomness
-        indices = np.arange(len(self.x_train))
-        np.random.shuffle(indices)
+        # # Shuffle the data to introduce randomness
+        # indices = np.arange(len(self.x_train))
+        # np.random.shuffle(indices)
 
         print(self.num_clients)
         # Split the data into num_clients parts
@@ -41,9 +37,12 @@ class Dataset :
         horizontal_clients_datasets = []
         for client in range(self.num_clients):
             client_index = client_IDs[client]
-            client_x = self.x_train[client_index]
-            client_y = self.y_train[client_index]
-            horizontal_clients_datasets.append((client_x, client_y))
+            client_x_train = self.x_train[client_index]
+            client_y_train = self.y_train[client_index]
+            client_x_test = self.x_test[client_index]
+            client_y_test = self.y_test[client_index]
+
+            horizontal_clients_datasets.append((client_x_train, client_y_train, client_x_test, client_y_test))
         return horizontal_clients_datasets
     
 
@@ -54,38 +53,39 @@ class Dataset :
         np.random.shuffle(indices)
 
 
-         #Data division for vertical FL
-        num_features = self.x_train.shape[1]
-        if self.num_clients >= num_features:
-            duplicate_features_needed = self.num_clients - num_features
-        else:
-            multiplier = num_features//self.num_clients + 1
-            duplicate_features_needed =  multiplier * self.num_clients - num_features
-
-        feature_names = list(range(num_features)) #Feature indicies without duplicates
+        #Data division for vertical FL
+        num_features = self.x_train.shape[1] #Here we refer to a feature as a row of pixels
         
-        for i in range(duplicate_features_needed):
-            x = random.randint(0, num_features -1)
-            feature_names.append(feature_names[x])
-
-        #Assign features to each client, such that each client has the same number of features
+        feature_names_indicies = list(range(num_features)) 
+        
+        #Assign features to each client, such that each client has around the same number of features
         clients_features = np.array_split(feature_names, self.num_clients)
 
         horizontal_clients_datasets = []
         for client in range(self.num_clients):
             client_features = clients_features[client]
-            client_x = self.x_train[:][client_features]
-            client_y = self.y_train[:][client_features]
-            horizontal_clients_datasets.append((client_x, client_y))
+            client_x_train = self.x_train[:][client_features]
+            client_y_train = self.y_train[:][client_features]
+            client_x_test = self.x_test[:][client_features]
+            client_y_test = self.y_test[:][client_features]
+            horizontal_clients_datasets.append((client_x_train, client_y_train, client_y_test, client_y_test))
         return horizontal_clients_datasets
 
-       
+
+    #Call this method to get your data as a client!
+    #vertical -> boolean
+    #clientID -> [0: numOfClients - 1]
+    def getMyData(self, vertical, clientID):
+        if vertical:
+            return self.vertical_clients_datasets(clientID)
+        else:
+            return self.horizontal_clients_datasets(clientID)
 
 
 
 
 
 
-data = Dataset(20)
-print(data.verticalDivideData())
+# data = Dataset(20)
+# print(data.verticalDivideData())
 
